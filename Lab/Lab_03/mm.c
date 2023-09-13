292cc5d644b4e307673833fd7917396360a6dfdd
c-jimmy1 <74884278+c-jimmy1@users.noreply.github.com>
2023-09-13 14:07:53 -0400

    created mult files

--- Lab/Lab_03/mm.c
+++ Lab/Lab_03/mm.c
@@ -19,11 +19,9 @@ CSCI-2500 Fall 2023
 
 
 
-int mm_alloc(matrix* mat);
 int mm_free (matrix* mat);
 int mm_print(matrix* mat);
 int mm_read (char* filename, matrix* mat);
-matrix* mm_matrix_mult(matrix* mat1, matrix* mat2);
 
 
 int mm_free(matrix* mat)
@@ -123,39 +121,6 @@ Notes:
   }
   
   return 0;
-}
-
-
-matrix* mm_matrix_mult(matrix* mat1, matrix* mat2)
-{
-/*
-Notes:
-  First check that it is actually possible to multiply the two inputs
-  If so, construct a result matrix
-  Then perform the multiplication
-  Then return a pointer to your result matrix
-*/
-  if (mat1->cols != mat2->rows) {
-    printf("mm_matrix_mult: dimension mismatch between matrices.\n");
-    exit(-1);
-  }
-  
-  matrix* result = (matrix*)malloc(sizeof(matrix));
-  result->rows = mat1->rows;
-  result->cols = mat2->cols;
-  mm_alloc(result);
-  
-  for(int i = 0; i < mat1->rows; ++i) {
-    for(int j = 0; j < mat2->cols; ++j) {
-      for(int k = 0; k < mat1->cols; ++k) {
-        // Note: because I called calloc() to initialize, I can
-        // directly place results into and update data[i][j]
-        result->data[i][j] +=  mat1->data[i][k] * mat2->data[k][j];
-      }
-    }
-  }
-  
-  return result;
 }
 
 int main()
---
+++ Lab/Lab_03/mult.h
@@ -0,0 +1,7 @@
+#ifndef MULT_H_   /* Include guard */
+#define MULT_H_
+#include "matrix.h"
+
+matrix* mm_matrix_mult(matrix* mat1, matrix* mat2);
+
+#endif
