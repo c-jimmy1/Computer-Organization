50927f31d08f462f4baef5dd9c3d5c6557ba7100
Jimmy Chen <jc219570512@gmail.com>
2023-10-27 13:24:54 -0400

    created function to read the example files

---
+++ Homework/Homework2/hw1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+void parseFile(const char *infileName) {
+    FILE *infile = fopen(infileName, "r");
+
+    if (infile == NULL) {
+        printf("Error: Cannot open file %s\n", infileName);
+        return 1;
+    }
+
+    fclose(infile);
+}
+
+int main(int argc, char *argv[]) {
+
+    // If the number of arguments is less than 2, print usage and exit
+    if (argc < 2) {
+        printf("Usage: %s <input file>\n", argv[0]);
+        return 1;
+    }
+
+    // Initialize the input file name and call the parseFile function
+    char *infileName = argv[1];
+    parseFile(infileName);
+
+    return 0;
+}
