2bc709321b3a4f53845db61459330ce7a9145dac
Jimmy Chen <jc219570512@gmail.com>
2023-10-30 02:34:01 -0400

    creating a struct to hold saved registers

--- Homework/Homework2/hw2.c
+++ Homework/Homework2/hw2.c
@@ -2,9 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
+
+/* Array of structs to hold the C variables, each struct has a char and an int. 
+    The char is the variable name and the int is the value*/
+    typedef struct {
+        char c;
+        int i;
+    } CharIntPair;
 
 
-void translatetoMIPS(file_string) {
+// bool checkFree(CharIntPair (*c_variables)[8][2]) {
+
+// }
+void translatetoMIPS(char *file_string, CharIntPair c_variables[8]) {
+    CharIntPair temporary_registers[10];
+    int current_temp_register = 0;
+    //printf("# %s", file_string);
+
+    int i = 0;
+    // Removing all spaces from the string
+    while (file_string[i] != '\0') {
+        if (file_string[i] == ' ') {
+            for (int j = i; file_string[j] != '\0'; j++) {
+                file_string[j] = file_string[j + 1];
+            }
+        }
+        i++;
+    }
+
+    // Looping through the string and checking for keywords
+    for (int j = 0; file_string[j] != '\0'; j++) {
+        if (file_string[i] == ';' && file_string[i + 1] == '\0') {
+            break;  // Exit the loop if a semicolon is at the end of the string
+        }
+        if (isalpha(file_string[i]) != 0 && file_string[i+1] == '=' && isalpha(file_string[i+2]) == 0) {
+            printf("# %d", file_string[i]);
+        }
+    }
+
+    c_variables[1].i = 5;
     
 }
 
@@ -15,14 +52,14 @@ void parseFile(const char *infileName) {
         printf("Error: Cannot open file %s\n", infileName);
         exit(1);
     }
-
-    char file_string[128];
+    char file_string[128]; 
     
+    CharIntPair c_variables[8];
     while (fgets(file_string, 128, infile) != NULL) {
         // File_string is each line and is passed into the translatetoMIPS function to be translated
-        translatetoMIPS(file_string);
+        translatetoMIPS(file_string, c_variables);
     }
-
+    printf("%d\n ", c_variables[1].i);
     fclose(infile);
 }
 
