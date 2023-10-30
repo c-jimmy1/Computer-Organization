e718f339163bea460d0d72c69d19029456f2a18e
Jimmy Chen <jc219570512@gmail.com>
2023-10-30 13:56:48 -0400

    implemented the variable declaration for mips

--- Homework/Homework2/hw2.c
+++ Homework/Homework2/hw2.c
@@ -6,22 +6,54 @@
 
 /* Array of structs to hold the C variables, each struct has a char and an int. 
     The char is the variable name and the int is the value*/
-    typedef struct {
-        char c;
-        int i;
-    } CharIntPair;
+typedef struct {
+    char variable;
+    int value;
+} CharIntPair;
 
+void printCharIntPairs(CharIntPair c_variables[8]) {
+    for (int i = 0; i < 8; i++) {
+        if (c_variables[i].variable != ' ') { // Check if the struct is initialized
+            printf("Index %d: Char = %c, Int = %d\n", i, c_variables[i].variable, c_variables[i].value);
+        }
+    }
+}
 
-// bool checkFree(CharIntPair (*c_variables)[8][2]) {
+// Function to check which saved register is free and returns the index of the free register
+int checkFree(CharIntPair c_variables[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (c_variables[i].variable == ' ') {
+            return i;
+        }
+    }
+    return -1;
+}
 
-// }
+// Function to declare a variable and assign it a value in the saved registers
+void declareVariable(CharIntPair c_variables[8], char variable, int value) {
+    // Loop through the array of structs and check if the variable has been declared
+    int index = checkFree(c_variables, 8);
+    if (index == -1) {
+        printf("Error: No free registers\n");
+        exit(1);
+    }
+    c_variables[index].variable = variable;
+    c_variables[index].value = value;
+    printf("li $s%d, %d\n", index, value);
+}
+
 void translatetoMIPS(char *file_string, CharIntPair c_variables[8]) {
+    // Intialize an array of structs to hold the temporary registers
     CharIntPair temporary_registers[10];
+    for (int i = 0; i < 8; i++) {
+        temporary_registers[i].variable = ' ';
+    }
     int current_temp_register = 0;
-    //printf("# %s", file_string);
+    
+    printf("# %s", file_string);
 
     int i = 0;
-    // Removing all spaces from the string
+    // Removing all spaces from the string to make it easier to parse
     while (file_string[i] != '\0') {
         if (file_string[i] == ' ') {
             for (int j = i; file_string[j] != '\0'; j++) {
@@ -31,18 +63,20 @@ void translatetoMIPS(char *file_string, CharIntPair c_variables[8]) {
         i++;
     }
 
-    // Looping through the string and checking for keywords
-    for (int j = 0; file_string[j] != '\0'; j++) {
-        if (file_string[i] == ';' && file_string[i + 1] == '\0') {
-            break;  // Exit the loop if a semicolon is at the end of the string
-        }
-        if (isalpha(file_string[i]) != 0 && file_string[i+1] == '=' && isalpha(file_string[i+2]) == 0) {
-            printf("# %d", file_string[i]);
+    // The first letter should always be a letter, and the second should always be an equals sign
+    if (isalpha(file_string[0]) != 0 && file_string[1] == '=') {
+        /* If the third character is a number, then it is a VARIABLE DECLARATION. 
+        Assume that a const can only be the second operand */
+        if (isalpha(file_string[2]) == 0) {
+            int index = 2; // Start parsing from index 2
+            int result = 0;
+            while (file_string[index] != ';') {
+                result = result * 10 + (file_string[index] - '0');
+                index++;
+            }
+            declareVariable(c_variables, file_string[0], result);
         }
     }
-
-    c_variables[1].i = 5;
-    
 }
 
 void parseFile(const char *infileName) {
@@ -55,11 +89,17 @@ void parseFile(const char *infileName) {
     char file_string[128]; 
     
     CharIntPair c_variables[8];
+    for (int i = 0; i < 8; i++) {
+        c_variables[i].variable = ' ';
+    }
+
     while (fgets(file_string, 128, infile) != NULL) {
         // File_string is each line and is passed into the translatetoMIPS function to be translated
         translatetoMIPS(file_string, c_variables);
     }
-    printf("%d\n ", c_variables[1].i);
+
+    // printCharIntPairs(c_variables);
+
     fclose(infile);
 }
 
