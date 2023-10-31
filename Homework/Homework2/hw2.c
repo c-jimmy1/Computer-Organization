9a11d6368925b2e218e224290901dd6eee1ac635
Jimmy Chen <jc219570512@gmail.com>
2023-10-31 03:23:12 -0400

    loaded declaration into saved register and printed

--- Homework/Homework2/hw2.c
+++ Homework/Homework2/hw2.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <ctype.h>
+
+int checkFree(char *array[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (array[i] == NULL) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int isNumeric(const char *str) {
+    char *endptr;  // Pointer to the character that caused conversion to stop
+    errno = 0;     // Reset errno for error checking
+
+    long num = strtol(str, &endptr, 10);  // Base 10 for decimal numbers
+
+    // Check for conversion errors
+    if ((errno == ERANGE && (num == LONG_MAX || num == LONG_MIN)) ||
+        (errno != 0 && num == 0)) {
+        return 0;  // Conversion failed
+    }
+
+    // Check if the entire string was consumed by strtol
+    if (*endptr != '\0') {
+        return 0;  // Not a valid integer (e.g., contains non-numeric characters)
+    }
+
+    return 1;  // Valid integer
+}
+
+void declareVariable(char *line[], char *c_variables[]) {
+    if (isalpha(*line[0]) != 0 && *line[1] == '=' && isNumeric(line[2])) {
+        int index = checkFree(c_variables, 8);
+        c_variables[index] = line[0];
+        printf("li $s%d, %s\n", index, line[2]);
+    }
+}
+
+void translatetoMIPS(char *line[], char *c_variables[]) {
+    // Initialize temp_registers array to NULL
+    char *temp_registers[10];
+    for (int i = 0; i < 10; i++) {
+        temp_registers[i] = NULL;
+    }
+    declareVariable(line, c_variables);
+}
 
 // Function to print the original C code line as a comment
 void printOriginal(char *line_string) {
@@ -10,16 +59,6 @@ void printOriginal(char *line_string) {
         }
         printf("# %s\n", line_string); 
 } 
-
-void translatetoMIPS(char *line[], char *c_variables[]) {
-    // Initialize temp_registers array to NULL
-    char *temp_registers[10];
-    for (int i = 0; i < 10; i++) {
-        temp_registers[i] = NULL;
-    }
-
-    
-}
 
 void parseFile(const char *infileName) {
     FILE *infile = fopen(infileName, "r");
@@ -40,6 +79,7 @@ void parseFile(const char *infileName) {
     // Read each line of the file using fgets, and tokenize the line by space, and convert the tokens to an Array
     while (fgets(line_string, 128, infile) != NULL) {        
         printOriginal(line_string);
+        line_string[strcspn(line_string, ";")] = 0; // Remove the semicolon from the line
         char *token = strtok(line_string, " "); // Tokenize the line by space
         int tokenCount = 0;
         char *line[128];
